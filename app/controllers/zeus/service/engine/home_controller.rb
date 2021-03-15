59cad9050d538d5f56a97ff073327a234ad82a5f
Eric Campbell <ericcampbell59@gmail.com>
2021-03-15 18:06:13 -0500

    skip authorize api request check for home controller

--- app/controllers/zeus/service/engine/home_controller.rb
+++ app/controllers/zeus/service/engine/home_controller.rb
@@ -1,4 +1,6 @@
 class Zeus::Service::Engine::HomeController < ApplicationController
+    skip_before_action :authorize_api_request!
+
     def index
         service_name = Rails.application.class.module_parent.name.clone.gsub("Service", "").downcase
         render json: {service: service_name}
