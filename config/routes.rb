2b729fc5113f0300b5b06e75887e75ef6766df41
Eric Campbell <ericcampbell59@gmail.com>
2021-03-22 09:04:32 -0500

    add defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Zeus::Service::Engine::Engine.routes.draw do
     root to: "home#index"
+
+    namespace :admin do
+        resources :project_environments
+    end
     
     namespace :api do
         namespace :v1 do
--- lib/zeus/service/engine/version.rb
+++ lib/zeus/service/engine/version.rb
@@ -1,7 +1,7 @@
 module Zeus
   module Service
     module Engine
-      VERSION = '0.3.1'
+      VERSION = '0.3.2'
     end
   end
 end
