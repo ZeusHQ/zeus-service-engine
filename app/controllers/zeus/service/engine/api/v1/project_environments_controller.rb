d561cfb9818ab1a378db6478b57be0e2706ffa36
Eric Campbell <ericcampbell59@gmail.com>
2021-04-01 10:29:14 -0500

    fix

--- app/controllers/zeus/service/engine/api/v1/project_environments_controller.rb
+++ app/controllers/zeus/service/engine/api/v1/project_environments_controller.rb
@@ -2,21 +2,23 @@ class Zeus::Service::Engine::Api::V1::ProjectEnvironmentsController < Zeus::Serv
     before_action :authorize_api_request!
     before_action :require_zeus_permissions!
 
+    CLASS_NAME = "Zeus::Service::Engine::ProjectEnvironment"
+
     def index
         environments = Zeus::Service::Engine::ProjectEnvironment.where(id: params[:ids]).all
-        render_resources(environments, "Zeus::Service::Engine::ProjectEnvironment")
+        render_resources(environments, CLASS_NAME)
     end
 
     def show
         env = Zeus::Service::Engine::ProjectEnvironment.find(params[:id])
-        render_resource(env.as_json(include_keys: true))
+        render_resource(env.as_json(include_keys: true), CLASS_NAME)
     end
 
     def create
         res = ProjectEnvironmentCommands::CreateProjectEnvironment.call(current_env, current_permissions, create_params)
 
         if res.success?
-            render_resource(res.payload.as_json(include_keys: true))
+            render_resource(res.payload.as_json(include_keys: true), CLASS_NAME)
         else
             render_error(res.errors)
         end
@@ -26,7 +28,7 @@ class Zeus::Service::Engine::Api::V1::ProjectEnvironmentsController < Zeus::Serv
         res = ProjectEnvironmentCommands::UpdateProjectEnvironment.call(current_env, current_permissions, params[:id], update_params)
 
         if res.success?
-            render_resource(res.payload)
+            render_resource(res.payload, CLASS_NAME)
         else
             render_error(res.errors)
         end
@@ -36,7 +38,7 @@ class Zeus::Service::Engine::Api::V1::ProjectEnvironmentsController < Zeus::Serv
         res = ProjectEnvironmentCommands::DestroyProjectEnvironment.call(current_env, current_permissions, params[:id])
 
         if res.success?
-            render_resource(res.payload)
+            render_resource(res.payload, CLASS_NAME)
         else
             render_error(res.errors)
         end
